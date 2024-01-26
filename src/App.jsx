345b9db51356473ae52dcd128db60dc09785c538
Alex <alex.som86@gmail.com>
2024-01-26 10:18:25 +0600

    add counter people

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Person from "./components/Person/Person";
 
 function App() {
   const [title, setTitle] = useState("Hello React"); // [data, fn]
+  const [people, setPeople] = useState([
+    { name: "Mike", age: 30, prof: "Motion Designer" },
+    { name: "John", age: 20, prof: "Junior Web Developer" },
+  ]);
 
   const changeTitle = () => {
     if (title === "Hello React") {
@@ -13,6 +17,26 @@ function App() {
     }
   };
 
+  const changeName = () => {
+    const copyPeople = [...people];
+    const person = { ...copyPeople[0] };
+    person.name = "Sam";
+    copyPeople[0] = person;
+
+    setPeople(copyPeople);
+  };
+
+  const increaseAge = () => {
+    setPeople((prevPeople) => {
+      return prevPeople.map((person) => {
+        return {
+          ...person,
+          age: person.age + 1,
+        };
+      });
+    });
+  };
+
   return (
     <div className="wrapper">
       <h1 className="title">{title}</h1>
@@ -20,13 +44,21 @@ function App() {
         <button className="btn" onClick={changeTitle}>
           Change title
         </button>
+
+        <button className="btn" onClick={changeName}>
+          Change Name
+        </button>
+
+        <button className="btn" onClick={increaseAge}>
+          Increase Age
+        </button>
       </div>
       <div className="list">
-        <Person name="Mike Jefferson" age={20}>
-          Профессия: <span>Motion Designer</span>
+        <Person name={people[0].name} age={people[0].age}>
+          Профессия: <span>{people[0].prof}</span>
         </Person>
-        <Person name="John Jefferson" age={30}>
-          Профессия: <span>Программист</span>
+        <Person name={people[1].name} age={people[1].age}>
+          Профессия: <span>{people[1].prof}</span>
         </Person>
       </div>
     </div>
